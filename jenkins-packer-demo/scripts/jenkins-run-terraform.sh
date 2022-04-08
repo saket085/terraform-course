38008aaf83525d8e34bcc94c7d6868ba74122451
saket085 <87834511+saket085@users.noreply.github.com>
2022-04-08 02:17:19 +0100

    Update jenkins-run-terraform.sh

--- jenkins-packer-demo/scripts/jenkins-run-terraform.sh
+++ jenkins-packer-demo/scripts/jenkins-run-terraform.sh
@@ -7,4 +7,5 @@ sed -i 's/terraform-state-xx70dpnh/'${S3_BUCKET}'/' backend.tf
 sed -i 's/#//g' backend.tf
 aws s3 cp s3://${S3_BUCKET}/amivar.tf amivar.tf --region $AWS_REGION
 terraform init
-terraform apply -auto-approve -var APP_INSTANCE_COUNT=1 -target aws_instance.app-instance
+#terraform apply -auto-approve -var APP_INSTANCE_COUNT=1 -target aws_instance.app-instance
+terraform plan
