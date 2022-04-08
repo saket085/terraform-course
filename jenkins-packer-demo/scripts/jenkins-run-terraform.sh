db01ec86a3f5442a0b5ea2d97ea87f1fd44f89f6
saket085 <87834511+saket085@users.noreply.github.com>
2022-04-08 01:33:29 +0100

    Update jenkins-run-terraform.sh

--- jenkins-packer-demo/scripts/jenkins-run-terraform.sh
+++ jenkins-packer-demo/scripts/jenkins-run-terraform.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 set -ex
 AWS_REGION="eu-west-2"
-rm -rf jenkins-packer-demo
-mkdir jenkins-packer-demo
 cd jenkins-packer-demo
 S3_BUCKET=`aws s3 ls --region $AWS_REGION |grep terraform-state |tail -n1 |cut -d ' ' -f3`
 sed -i 's/terraform-state-xx70dpnh/'${S3_BUCKET}'/' backend.tf
